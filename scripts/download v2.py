240c7728c4b77741ece38658f35b320615f0f7b4
ajayman0har <56766439+ajayman0har@users.noreply.github.com>
2025-10-22 14:26:04 -0400

    Update download v2.py

--- scripts/download v2.py
+++ scripts/download v2.py
@@ -12,30 +12,30 @@ BATCH_SIZE = 50    # searching by 50s (debugging)
 
 def fetch_sequences(lineage_name, lineage_term):
     print(f"Searching {lineage_name}")
-    base_term = (f"Severe acute respiratory syndrome coronavirus 2[Organism] AND human[host] AND 29000:31000[Sequence Length] AND {lineage_term}")
+    base_term = (f"Severe acute respiratory syndrome coronavirus 2[Organism] AND human[host] AND 29000:31000[Sequence Length] AND {lineage_term} AND USA[Country]")
 
     # Search for IDs
     handle = Entrez.esearch(db="nucleotide", term=base_term, retmax=MAX_RESULTS)
-    result = Entrez.read(handle)
-    handle.close()
-    ids = result["IdList"]
+    result = Entrez.read(handle) #turns in to dictionary
+    handle.close()  #cut connection to NCBI
+    ids = result["IdList"] #grabs assecion numbers
     print(f"Found {len(ids)} records for {lineage_name}")
 
     n_proteins, metadata = [], []
 
     # Process IDs 
-    for i in range(0, len(ids), BATCH_SIZE):
-        batch_ids = ids[i:i+BATCH_SIZE]
+    for i in range(0, len(ids), BATCH_SIZE):  #runs through all ids
+        batch_ids = ids[i:i+BATCH_SIZE] #50 at a time
         try:
-            handle = Entrez.efetch(db="nucleotide", id=batch_ids, rettype="gb", retmode="text")
+            handle = Entrez.efetch(db="nucleotide", id=batch_ids, rettype="gb", retmode="text") #gb gives annotiations, text allows seq io to read, searching nucleotide database
             for record in SeqIO.parse(handle, "genbank"):
                 # Extract N protein
-                for f in record.features:
-                    if f.type == "CDS" and "nucleocapsid" in f.qualifiers.get("product", [""])[0].lower():
-                        seq = f.qualifiers.get("translation", [""])[0]
-                        if not seq or len(seq) < 300:
+                for f in record.features: 
+                    if f.type == "CDS" and "nucleocapsid" in f.qualifiers.get("product", [""])[0].lower(): #if its a coding sequence and the coding sequence gives nucleocapsid:
+                        seq = f.qualifiers.get("translation", [""])[0] #qualifiers are additional information
+                        if not seq or len(seq) < 300: # if too short
                             continue
-                        if "X" in seq or "?" in seq:
+                        if "X" in seq or "?" in seq: #  if unknown AA
                             continue
 
                         n_proteins.append(f">{record.id}|{lineage_name} {record.description}\n{seq}\n")
@@ -64,4 +64,5 @@ pd.DataFrame(all_metadata).to_csv("sarscov2_N_metadata_21J_22C_subset.csv", inde
 
 print("\nSaved sarscov2_N_proteins_21J_22C_subset.fasta")
 print("Saved sarscov2_N_metadata_21J_22C_subset.csv")
+
 
